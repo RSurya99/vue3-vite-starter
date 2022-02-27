6fb784d92180c5a907f0be7b5eb99845226b33cb
RSurya99 <rafli.rfsp@gmail.com>
2022-02-27 16:31:13 +0700

    chore: setup tailwind-config-viewer, update basic style, and add simple usage of pinia

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
     <h1>This is an about page</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import { toggleDark } from '@/composables'
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore()
+
+const counter = computed(() => counterStore.counter)
+const doubleCount = computed(() => counterStore.doubleCount)
+
+const increment = () => counterStore.increment()
 </script>
 <template>
   <div>
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
     <h1 class="text-gray-700 dark:text-indigo-500">This is home page</h1>
+
+    <div>
+      <h2>Counter</h2>
+      <p>counter : {{ counter }}</p>
+      <p>doubleCount : {{ doubleCount }}</p>
+      <button @click="increment">Increment</button>
+    </div>
 
     <button class="p-2 bg-gray-50 shadow rounded" @click="toggleDark()">
       <IconMdiBrightness6 />
